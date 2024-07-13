e8a76197c620e262cfcfab6696ea5e046f5b38c6
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 06:00:43 +0000

    Variable start

--- Revesion/Class1/script.js
+++ Revesion/Class1/script.js
@@ -1,9 +1,21 @@
-console.log("Hello World")
-// alert("Hey this is me")
-
+// 1. Way to print in javaScript
+/*
+alert("Hey this is me")
 document.getElementsByTagName('h1')[0].style.color = "Green";
 document.write("This is document write");
+*/
 
+// // 2. console API
+/*
+console.log("Hello World")
 console.warn("This is warning");
 console.error("This is an error")
 console.assert(4 != 4)
+*/
+
+
+// 3.   JavaScript Variable
+var num1 = 30;
+var num2 = 90;
+
+console.log(num1 + num2)
