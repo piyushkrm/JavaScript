25bf3d420b194caa746cbf9a2d3f567da53d9e54
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 10:10:57 +0000

    Date done

--- Revesion/String.js
+++ Revesion/String.js
@@ -5,3 +5,15 @@ console.log("Index value of last best : " + str1.lastIndexOf("best"));
 
 console.log(str1.slice(4, 7));
 console.log(str1.replace("best best", "very best"));
+
+
+// Date and Time
+let date = new Date();
+console.log(date);
+console.log(date.getTime());
+console.log(date.getFullYear());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getHours());
+console.log(date.getTimezoneOffset());
+console.log(date.getDate());
