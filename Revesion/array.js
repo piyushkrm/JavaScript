bc0a138f118c938c8e7d1d71e9415de5c71ce7c5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 08:40:39 +0000

    Add element in the array at the last of an array

--- Revesion/array.js
+++ Revesion/array.js
@@ -5,3 +5,8 @@ console.log(array);
 // delete last element of an array
 console.log(array.pop());
 console.log(array);
+
+
+// add element at the end of an array
+console.log(array.push("Sona"));
+console.log(array);
