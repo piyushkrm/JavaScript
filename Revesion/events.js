487c34ebb7265b4e1a073f1bbf15da6d8f07e8e7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 11:34:57 +0000

    Event listener are done

--- Revesion/events.js
+++ Revesion/events.js
@@ -1,1 +1,37 @@
 // EVENTS IN JS
+function clicked() {
+    alert("You clciked on button")
+}
+
+// window.onload = function() {
+//     alert("Documents are loading slowly slowly")
+// }
+
+
+// events
+
+events = document.querySelector("#even");
+events.addEventListener('click', function() {
+    console.log("Button was clicked")
+})
+
+events.addEventListener('mouseover', function() {
+    console.log("Mouse on Container");
+})
+
+events.addEventListener('mouseout', function() {
+    console.log("Mouse out of container");
+})
+
+events.addEventListener("mouseup", function() {
+    console.log("Mouse up when clicked on container");
+})
+
+events.addEventListener("mousedown", function() {
+    console.log("Mouse down when clicked on container");
+})
+
+events.addEventListener('click', function() {
+    document.querySelectorAll('.con')[1].innerHTML = "<i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias</i>"
+    console.log("Button was clicked")
+})
