cfd9dd6fe8064efd1e3650a83b0ca7ccd0bf2fb0
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 06:47:45 +0000

    Now conditional statement

--- Revesion/Function.js
+++ Revesion/Function.js
@@ -1,1 +1,8 @@
-console.log("This is function script file")
+// DRY : Do not Repeat Yourself
+function avg(a, b) {
+    return (a + b) / 2;
+}
+
+p1 = avg(10, 15);
+console.log(p1)
+console.log(avg(45, 56));
