bf183439c7c104641426ab48bcbbb16995921139
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 06:27:29 +0000

    Arithmetic operator

--- Revesion/Class1/script.js
+++ Revesion/Class1/script.js
@@ -23,18 +23,24 @@ console.log(num1 + num2)
 
 // 4.   Data types in JavaScript
 // Number
+/*
 var num = 2406;
 var num3 = 24.06;
 console.log(typeof(num))
 console.log(typeof(num3))
+*/
+
 
 // String
+/*
 var str1 = "This is a string";
 var str2 = "This is also a string";
 console.log(typeof(str1))
 console.log(typeof(str2))
+*/
 
 // Objects
+/*
 var marks =  {
     Piyush : 90,
     aditys : 98,
@@ -43,18 +49,21 @@ var marks =  {
 }
 console.log(typeof(marks))
 console.log(marks)
+*/
 
 // booleans
+/*
 var a = true;
 var b = false;
 console.log(typeof(a));
 console.log(a)
 console.log(b)
-
+*/
 
 // undefined
+/*
  var p;console.log(p)
-
+*/
 
 /*
 At a very high level, there are two types of data types in javaScript :- 
@@ -63,5 +72,18 @@ At a very high level, there are two types of data types in javaScript :-
 */
 
 // Arrays
+/*
 var array = [1, 2, 5, 9, "Piyush", "Abhi", 'sona'];
 console.log(array);
+console.log(array[3]); */
+
+
+// Operator in JavaScript
+console.log("Arithmetic Operator");
+var x = 54;
+var y = 36;
+console.log("The value of a + b  is  : ", x + y);
+console.log("The value of a - b  is  : ", x - y);
+console.log("The value of a * b  is  : ", x * y);
+console.log("The value of a / b  is  : ", x / y);
+console.log("The value of a % b  is  : ", x % y);
