4f9ffd182efe546ca9c7d0c57a3c8e973a1e3b90
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-07-13 09:14:53 +0000

    String methods are done

---
+++ Revesion/String.js
@@ -0,0 +1,7 @@
+let str1 = "I am the best best";
+console.log("Length of a string is  : " + str1.length);
+console.log("Index value of best : " + str1.indexOf("best"));
+console.log("Index value of last best : " + str1.lastIndexOf("best"));
+
+console.log(str1.slice(4, 7));
+console.log(str1.replace("best best", "very best"));
--- Revesion/script.js
+++ Revesion/script.js
@@ -1,46 +1,35 @@
 // 1. Way to print in javaScript
-/*
 alert("Hey this is me")
 document.getElementsByTagName('h1')[0].style.color = "Green";
 document.write("This is document write");
-*/
 
 // // 2. console API
-/*
 console.log("Hello World")
 console.warn("This is warning");
 console.error("This is an error")
 console.assert(4 != 4)
-*/
 
 
 // 3.   JavaScript Variable
-/*
 var num1 = 30;
 var num2 = 90;
 console.log(num1 + num2)
-*/
 
 // 4.   Data types in JavaScript
 // Number
-/*
 var num = 2406;
 var num3 = 24.06;
 console.log(typeof(num))
 console.log(typeof(num3))
-*/
 
 
 // String
-/*
 var str1 = "This is a string";
 var str2 = "This is also a string";
 console.log(typeof(str1))
 console.log(typeof(str2))
-*/
 
 // Objects
-/*
 var marks =  {
     Piyush : 90,
     aditys : 98,
@@ -49,21 +38,16 @@ var marks =  {
 }
 console.log(typeof(marks))
 console.log(marks)
-*/
 
 // booleans
-/*
 var a = true;
 var b = false;
 console.log(typeof(a));
 console.log(a)
 console.log(b)
-*/
 
 // undefined
-/*
  var p;console.log(p)
-*/
 
 /*
 At a very high level, there are two types of data types in javaScript :- 
@@ -72,14 +56,12 @@ At a very high level, there are two types of data types in javaScript :-
 */
 
 // Arrays
-/*
 var array = [1, 2, 5, 9, "Piyush", "Abhi", 'sona'];
 console.log(array);
-console.log(array[3]); */
+console.log(array[3]); 
 
 
 // Operator in JavaScript
-/*
 console.log("Arithmetic Operator");
 var x = 54;
 var y = 36;
@@ -100,10 +82,8 @@ console.log(x < y);
 console.log(x == y);
 console.log(x >= y);
 console.log(x <= y);
-*/
 
 // Logical Operator
-/*
 console.log("AND logical operator")
 console.log(true && true);
 console.log(true && false);
@@ -120,5 +100,3 @@ console.log(false || false);
 console.log("Logical Not operator");
 console.log(!true);
 console.log(!false);
-*/
- 
